0821ee93684ed1c830bdda41fe5d4161fbbb6d08
marco <marcoreda56@gmail.com>
2026-03-27 18:28:53 +0200

    the dark mode is work

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,14 @@
   <div class="app-container min-h-screen bg-gray-50">
     <AppNavbar v-if="!$route.meta.hideNavbar" />
 
-    <main :class="[!$route.meta.hideNavbar ? 'w-full max-w-[1800px] mx-auto pt-12 pb-8 px-4 md:px-8' : 'w-full']">
-    <router-view v-slot="{ Component }">
+    <main
+      :class="[
+        !$route.meta.hideNavbar
+          ? 'w-full max-w-[1800px] mx-auto pt-12 pb-8 px-4 md:px-8'
+          : 'w-full',
+      ]"
+    >
+      <router-view v-slot="{ Component }">
         <transition name="fade" mode="out-in">
           <component :is="Component" />
         </transition>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,14 @@
+/** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: 'class',
+
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        jakarta: ['Plus Jakarta Sans', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
